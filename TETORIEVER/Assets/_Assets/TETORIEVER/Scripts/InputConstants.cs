8ccb55f3eee79b45215f2f4dcd7a83cd3c85fb71
SilCil <65537555+SilCil@users.noreply.github.com>
2020-12-06 21:58:59 +0900

    Add: InputConstants

---
+++ TETORIEVER/Assets/_Assets/TETORIEVER/Scripts/InputConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace TETORIEVER
+{
+    public static class InputConstants
+    {
+        public const string Place = "Place";
+        public const string Rotate = "Rotate";
+        public const string Hand1 = "Hand1";
+        public const string Hand2 = "Hand2";
+        public const string Hand3 = "Hand3";
+        public const string Hand4 = "Hand4";
+    }
+}
